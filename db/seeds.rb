1f810d3507da7378195d327b8af0cc969671caa3
Caio <caio.dscamara@gmail.com>
2018-07-14 17:06:39 -0400

    Seeds for kinds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+['Débito', 'Juros', 'Financiamento', 'Crédito', 'Empréstimo'].each do |type|
+  Kind.create!(:description => type)
+end
