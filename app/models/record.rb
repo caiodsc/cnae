210ac75d9ab6f1aa8c85c345a87e9092ba919aca
Caio <caio.dscamara@gmail.com>
2018-07-14 23:38:23 -0400

    Validation 1

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,4 +2,5 @@ class Record < ApplicationRecord
   belongs_to :kind
   register_currency :brl
   monetize :amount_cents
+  validates :kind_id, acceptance: { accept: [1, 4], message: 'kind must be 1 or 4' }
 end
